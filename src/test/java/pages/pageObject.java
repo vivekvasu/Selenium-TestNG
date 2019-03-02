8e37273c05c34c957b03f6c2384dfd1557c5c719
vivekvasu <vivekvivi39@gmail.com>
2019-03-02 19:33:51 +0530

    Updated code

--- src/test/java/pages/pageObject.java
+++ src/test/java/pages/pageObject.java
@@ -1,13 +1,31 @@
 package pages;
 
+import java.lang.reflect.Field;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+
+import tests.BaseTests;
 
 public class pageObject 
 {
 	public pageObject(WebDriver wDriver) 
 	{
 		PageFactory.initElements(wDriver, this);
+		try 
+		{
+			Field[] fields = this.getClass().asSubclass(this.getClass()).getDeclaredFields();
+			for (int i = 0; i < fields.length; i++) 
+			{
+				BaseTests.elementsMap.put(fields[i].get(this).toString(), fields[i].getName());
+			}
+		} catch (IllegalArgumentException e) 
+		{
+			e.printStackTrace();
+		} catch (IllegalAccessException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
